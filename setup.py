9a9e6c91757fc5d59ada3fdabb4dd6c0ff9c89e6
dhiraj dhule <dhirajdhule@gmail.com>
2016-10-21 11:06:28 +0530

    removed license

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='pyzmp_tutorials',
       url='http://github.com/asmodehn/pyzmp',
       author='Dhirajd',
       author_email='dhirajdhule@gmail.com',
-      license='BSD',
       packages=['pyzmp_tutorials'],
       zip_safe=False
       )
