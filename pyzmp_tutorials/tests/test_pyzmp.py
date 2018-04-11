86e8cea7d505268448622503e3f7aa5213e62dcd
AlexV <asmodehn@gmail.com>
2018-04-11 17:22:40 +0900

    cosmetics for tests

--- pyzmp_tutorials/tests/test_pyzmp.py
+++ pyzmp_tutorials/tests/test_pyzmp.py
@@ -6,46 +6,54 @@ import time
 from pyzmp_tutorials import server_node, client_node
 
 
-srv = None
-vrs = None
-try:
+def test_request_response():
+    srv = None
+    vrs = None
+    try:
 
-    # watcher = pyzmp.process_watcher()
+        # watcher = pyzmp.process_watcher()
 
-    time.sleep(5)  # to give time to test user interruption
+        time.sleep(5)  # to give time to test user interruption
 
-    srv = server_node.ServerNode("srv")
-    srv.start()
+        srv = server_node.ServerNode("srv")
+        srv.start()
 
-    time.sleep(5)  # to give time to test user interruption
+        time.sleep(5)  # to give time to test user interruption
 
-    vrs = client_node.ClientNode("vrs")
-    vrs.start()
+        vrs = client_node.ClientNode("vrs")
+        vrs.start()
 
-    question = pyzmp.discover("question")
+        question = pyzmp.discover("question")
 
-    time.sleep(5)  # to give time to test user interruption
+        time.sleep(5)  # to give time to test user interruption
 
-    response = pyzmp.discover("response")
-    print(question.call())
+        response = pyzmp.discover("response")
+        print(question.call())
 
-    time.sleep(5)  # to give time to test user interruption
+        time.sleep(5)  # to give time to test user interruption
 
-    print(response.call())
+        print(response.call())
 
-except KeyboardInterrupt:
-    print("Shutdown requested by user")
+    except KeyboardInterrupt:
+        print("Shutdown requested by user")
 
-finally:
-    if srv:
-        srv.shutdown()
+    finally:
+        if srv:
+            srv.shutdown()
 
-    time.sleep(5)  # to give time to test user interruption
+        time.sleep(5)  # to give time to test user interruption
 
-    if vrs:
-        vrs.shutdown()
+        if vrs:
+            vrs.shutdown()
 
-    time.sleep(5)  # to give time to test user interruption
+        time.sleep(5)  # to give time to test user interruption
 
-    # if watcher:
-    #     watcher.terminate()
+        # if watcher:
+        #     watcher.terminate()
+
+# Just in case we run this directly
+if __name__ == '__main__':
+    import pytest
+    pytest.main([
+        '-s', __file__,
+])
--- pyzmp_tutorials/tests/test_pyzmp_cm.py
+++ pyzmp_tutorials/tests/test_pyzmp_cm.py
@@ -6,15 +6,24 @@ import time
 
 from pyzmp_tutorials import server_node, client_node
 
-# with pyzmp.process_watcher_cm():
-with server_node.ServerNode("srv"):
-    question = pyzmp.discover("question")
-    print(question.call())
-    time.sleep(2)  # to give time to test user interruption
 
-    with client_node.ClientNode("vrs"):
-        response = pyzmp.discover("response")
-
+def test_request_response_ctxtmgr():
+    # with pyzmp.process_watcher_cm():
+    with server_node.ServerNode("srv"):
+        question = pyzmp.discover("question")
+        print(question.call())
         time.sleep(2)  # to give time to test user interruption
-        print(response.call())
 
+        with client_node.ClientNode("vrs"):
+            response = pyzmp.discover("response")
+
+            time.sleep(2)  # to give time to test user interruption
+            print(response.call())
+
+
+# Just in case we run this directly
+if __name__ == '__main__':
+    import pytest
+    pytest.main([
+        '-s', __file__,
+])
